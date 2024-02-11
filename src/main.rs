5b8b726961025c0dba379240b32a683428d3e91e
onon1101 <mingzhuguly@gmail.com>
2024-02-11 20:20:42 +0800

    got integration tests working

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,14 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
+use blog_os::println;
 
 pub trait Testable {
     fn run(&self) -> ();
-}
-
-impl<T> Testable for T 
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
 }
 
 #[cfg(not(test))]
@@ -51,10 +22,7 @@ fn panic(info: &PanicInfo) -> !{
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> !{
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
+    blog_os::test_panic_handler(info)
 }
 
  #[no_mangle]
@@ -67,17 +35,3 @@ fn panic(info: &PanicInfo) -> !{
     loop {}
  }
 
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1,1);
-}
