f5bd12d79c242beb35fab679c539fac1b1382909
onon1101 <mingzhuguly@gmail.com>
2024-02-13 14:21:12 +0800

    create IDT in interrupt.rs

---
+++ src/interrupts.rs
@@ -0,0 +1,17 @@
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use crate::println;
+
+static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+
+pub fn init_idt() {
+    unsafe {
+        IDT.breakpoint.set_handler_fn(breakpoint_handler);
+        IDT.load();
+    }
+}
+extern "x86-interrupt" fn breakpoint_handler(
+    stack_frame: InterruptStackFrame
+    )
+{
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
